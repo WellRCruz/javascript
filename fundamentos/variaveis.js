6565bb3cd035c9749a77a8b24d25d5bdd76147ac
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2024-09-13 21:38:52 -0300

    estudos das variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ * Estudo de constante e variáveis
+ * @author Wellington R. Cruz
+ */
+
+console.clear()
+
+//variaveis
+let nome 
+let idade
+let vip
+
+nome = "Wellington R.Cruz"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 28
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
