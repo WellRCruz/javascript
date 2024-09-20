020b1e9942cb3a7d8cb6b5962c26f496897d4a60
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2024-09-19 22:17:34 -0300

    estudos das variáveis

---
+++ exercicios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha de um aluno de academia
+ * @Author Wellington R. Cruz
+ */
+
+// variáveis
+let nome 
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+
+// entrada de dados
+nome = "Wellington R. Cruz"
+idade = 28
+peso = 81
+altura = 1.81
+vip = true
+
+//saída
+console.log("ficha do aluno")
+console.log("______________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`VIP: ${vip}`)
