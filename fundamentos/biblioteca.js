64e362b460a55bd1f0e91b09e69757ed5b179222
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2024-09-26 22:23:47 -0300

    Pacotes/Biblioteca

---
+++ fundamentos/biblioteca.js
@@ -0,0 +1,12 @@
+/**
+ * Exemplo de uso de pacotes externos ("bibliotecas")
+ * @author Wellington R. Cruz
+*/
+
+// importar o pacote
+const colors = require ('colors')
+
+console.clear()
+
+console.log("Hello World".blue.bold)
+
