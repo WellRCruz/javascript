df1f0dd7ca69ba5c3e9fcd87c762721090e4f72d
WELLINGTON RODRIGO CRUZ <wellington.rcruz@senacsp.edu.br>
2024-10-31 22:30:48 -0300

    HTML e avaliação

---
+++ htmljs/script.js
@@ -0,0 +1,10 @@
+/**
+ * Hello JS
+ * Exemplo de ligação do documento html com o JS
+ * @author Wellington R. Cruz
+ */
+
+function helloJS() {
+    let nome = prompt ("Digite o seu nome: ")
+    alert(`Hello ${nome}`)
+}
